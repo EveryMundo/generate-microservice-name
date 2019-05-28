5de0fd13f0a739fb6757ca63f3a11167b4c61fd5
Daniel Santana <github@danielsantana.net>
2019-05-28 14:40:02 -0400

    standardize linting with standard.js

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
 // this will set the global.__rootdir in cases it is not yet set
-require('@everymundo/global-root-dir').setGlobalRootDir();
+require('@everymundo/global-root-dir').setGlobalRootDir()
 
-const { getPackageJSON } = require('./lib/get-package-json');
+const { getPackageJSON } = require('./lib/get-package-json')
 
-const nameRegExp = /^[-\w]+-v\d+$/;
+const nameRegExp = /^[-\w]+-v\d+$/
 
 const getMajorVersionNumber = (appPackageJson = getPackageJSON()) => {
-  const { version } = appPackageJson;
-  return +version.substr(0, version.indexOf('.'));
-};
+  const { version } = appPackageJson
+  return +version.substr(0, version.indexOf('.'))
+}
 
 const getPrefixedMajorVersionNumber = (appPackageJson = getPackageJSON()) => {
-  const { version } = appPackageJson;
-  const majorVersionNumber = getMajorVersionNumber({ version });
-  return `v${majorVersionNumber}`;
-};
+  const { version } = appPackageJson
+  const majorVersionNumber = getMajorVersionNumber({ version })
+  return `v${majorVersionNumber}`
+}
 
 const getCleanAppName = (appPackageJson = getPackageJSON()) => {
-  const { name } = appPackageJson;
-  return name.substr(name.lastIndexOf('/') + 1);
-};
+  const { name } = appPackageJson
+  return name.substr(name.lastIndexOf('/') + 1)
+}
 
 const generateMicroserviceNameFrom = ({ name, version }) => {
   // const majorVersionNumber = +version.substr(0, version.indexOf('.'));
-  const prefixedMajorVersionNumber = getPrefixedMajorVersionNumber({ version });
+  const prefixedMajorVersionNumber = getPrefixedMajorVersionNumber({ version })
   // const cleanName          = name.substr(name.lastIndexOf('/') + 1);
-  const cleanName          = getCleanAppName({ name });
+  const cleanName = getCleanAppName({ name })
 
   // const appName = `${cleanName}-v${majorVersionNumber}`;
-  const appName = `${cleanName}-${prefixedMajorVersionNumber}`;
+  const appName = `${cleanName}-${prefixedMajorVersionNumber}`
 
-  return appName;
-};
-
+  return appName
+}
 
 const itHasAValidCustomMicroserviceName = ({ config } = {}) =>
-  Boolean(config) && nameRegExp.test(config.customMicroserviceName);
-
+  Boolean(config) && nameRegExp.test(config.customMicroserviceName)
 
 const getMicroserviceName = () => {
-  const appPackageJson = getPackageJSON();
+  const appPackageJson = getPackageJSON()
 
   if (itHasAValidCustomMicroserviceName(appPackageJson)) {
-    return appPackageJson.config.customMicroserviceName;
+    return appPackageJson.config.customMicroserviceName
   }
 
-  return generateMicroserviceNameFrom(appPackageJson);
-};
+  return generateMicroserviceNameFrom(appPackageJson)
+}
 
 module.exports = {
   getPrefixedMajorVersionNumber,
@@ -56,5 +54,5 @@ module.exports = {
   getMicroserviceName,
   itHasAValidCustomMicroserviceName,
   getPackageJSON,
-  nameRegExp,
-};
+  nameRegExp
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,202 +1,201 @@
-'require strict';
+'require strict'
 
 /* eslint-disable no-unused-expressions */
 
-const
-  sinon = require('sinon'),
-  path  = require('path'),
-  cleanrequire = require('@everymundo/cleanrequire'),
-  { expect } = require('chai');
+const sinon = require('sinon')
+const path = require('path')
+const { expect } = require('chai')
+const cleanrequire = require('@everymundo/cleanrequire')
 
-require('@everymundo/global-root-dir').setGlobalRootDir(path.dirname(__dirname));
+require('@everymundo/global-root-dir').setGlobalRootDir(path.dirname(__dirname))
 
 describe('index.js', () => {
-  let box;
-  beforeEach(() => { box = sinon.sandbox.create(); });
+  let box
+  beforeEach(() => { box = sinon.createSandbox() })
 
   // retores the sandbox
-  afterEach(() => { box.restore(); });
+  afterEach(() => { box.restore() })
 
   describe('#getPackageJSON', () => {
     it('should return this package.json', () => {
-      const { getPackageJSON } = require('../index');
+      const { getPackageJSON } = require('../index')
 
-      const expected = require('../package.json');
-      const res = getPackageJSON();
+      const expected = require('../package.json')
+      const res = getPackageJSON()
 
-      expect(res).to.equal(expected);
-    });
-  });
+      expect(res).to.equal(expected)
+    })
+  })
 
   describe('#getMajorVersionNumber', () => {
-    const { getMajorVersionNumber } = require('../index');
+    const { getMajorVersionNumber } = require('../index')
 
     context('passing a version', () => {
       it('should load package.json', () => {
-        const res = getMajorVersionNumber({version: '2.3.4'});
+        const res = getMajorVersionNumber({ version: '2.3.4' })
 
-        expect(res).to.equal(2);
-      });
-    });
+        expect(res).to.equal(2)
+      })
+    })
 
     context('passing NO arguments', () => {
       it('should load package.json', () => {
-        const expected = +require('../package.json').version.split('.').shift();
-        const res = getMajorVersionNumber();
+        const expected = +require('../package.json').version.split('.').shift()
+        const res = getMajorVersionNumber()
 
-        expect(res).to.equal(expected);
-      });
-    });
-  });
+        expect(res).to.equal(expected)
+      })
+    })
+  })
 
   describe('#getPrefixedMajorVersionNumber', () => {
-    const { getPrefixedMajorVersionNumber } = require('../index');
+    const { getPrefixedMajorVersionNumber } = require('../index')
 
     context('passing a version', () => {
       it('should load package.json', () => {
-        const res = getPrefixedMajorVersionNumber({version: '2.3.4'});
+        const res = getPrefixedMajorVersionNumber({ version: '2.3.4' })
 
-        expect(res).to.equal('v2');
-      });
-    });
+        expect(res).to.equal('v2')
+      })
+    })
 
     context('passing NO arguments', () => {
       it('should load package.json', () => {
-        const expected = `v${require('../package.json').version.split('.').shift()}`;
-        const res = getPrefixedMajorVersionNumber();
+        const expected = `v${require('../package.json').version.split('.').shift()}`
+        const res = getPrefixedMajorVersionNumber()
 
-        expect(res).to.equal(expected);
-      });
-    });
-  });
+        expect(res).to.equal(expected)
+      })
+    })
+  })
 
   describe('#getCleanAppName', () => {
-    const { getCleanAppName } = require('../index');
+    const { getCleanAppName } = require('../index')
 
     context('passing a simple name', () => {
       it('should load package.json', () => {
-        const res = getCleanAppName({name: 'simple-app'});
+        const res = getCleanAppName({ name: 'simple-app' })
 
-        expect(res).to.equal('simple-app');
-      });
-    });
+        expect(res).to.equal('simple-app')
+      })
+    })
 
     context('passing a name with namespace', () => {
       it('should load package.json', () => {
-        const res = getCleanAppName({name: '@namespace/namespaced-name'});
+        const res = getCleanAppName({ name: '@namespace/namespaced-name' })
 
-        expect(res).to.equal('namespaced-name');
-      });
-    });
+        expect(res).to.equal('namespaced-name')
+      })
+    })
 
     context('passing NO arguments', () => {
       it('should load package.json', () => {
-        const expected = require('../package.json').name.split('/').pop();
-        const res = getCleanAppName();
+        const expected = require('../package.json').name.split('/').pop()
+        const res = getCleanAppName()
 
-        expect(res).to.equal(expected);
-      });
-    });
-  });
+        expect(res).to.equal(expected)
+      })
+    })
+  })
 
   describe('#generateMicroserviceNameFrom', () => {
     context('when name has a workspace like @everymundo/the-name', () => {
       it('should return the correct generated name', () => {
-        const { generateMicroserviceNameFrom } = require('../index');
+        const { generateMicroserviceNameFrom } = require('../index')
 
-        const input = { name: '@namespace/app-name', version: '2.3.4' };
-        const res = generateMicroserviceNameFrom(input);
-        const expected = 'app-name-v2';
+        const input = { name: '@namespace/app-name', version: '2.3.4' }
+        const res = generateMicroserviceNameFrom(input)
+        const expected = 'app-name-v2'
 
-        expect(res).to.equal(expected);
-      });
-    });
+        expect(res).to.equal(expected)
+      })
+    })
 
     context('when name does NOT HAVE a workspace', () => {
       it('should return the correct generated name', () => {
-        const { generateMicroserviceNameFrom } = require('../index');
+        const { generateMicroserviceNameFrom } = require('../index')
 
-        const input = { name: 'some-app-name', version: '1.2.3.4' };
-        const res = generateMicroserviceNameFrom(input);
-        const expected = 'some-app-name-v1';
+        const input = { name: 'some-app-name', version: '1.2.3.4' }
+        const res = generateMicroserviceNameFrom(input)
+        const expected = 'some-app-name-v1'
 
-        expect(res).to.equal(expected);
-      });
-    });
-  });
+        expect(res).to.equal(expected)
+      })
+    })
+  })
 
   describe('#itHasAValidCustomMicroserviceName', () => {
-    const { itHasAValidCustomMicroserviceName } = require('../index');
+    const { itHasAValidCustomMicroserviceName } = require('../index')
 
     context('when it DOES NOT have a custom name', () => {
       it('should return false', () => {
-        const res = itHasAValidCustomMicroserviceName();
+        const res = itHasAValidCustomMicroserviceName()
 
-        expect(res).to.be.false;
-      });
-
-      it('should return false', () => {
-        const input = {};
-        const res = itHasAValidCustomMicroserviceName(input);
-
-        expect(res).to.be.false;
-      });
+        expect(res).to.be.false
+      })
 
       it('should return false', () => {
-        const input = {config: {}};
-        const res = itHasAValidCustomMicroserviceName(input);
+        const input = {}
+        const res = itHasAValidCustomMicroserviceName(input)
 
-        expect(res).to.be.false;
-      });
-    });
+        expect(res).to.be.false
+      })
+
+      it('should return false', () => {
+        const input = { config: {} }
+        const res = itHasAValidCustomMicroserviceName(input)
+
+        expect(res).to.be.false
+      })
+    })
 
     context('when it has a custom name', () => {
       context('and it is Valid', () => {
         it('should return true', () => {
           const input = {
             config: {
-              customMicroserviceName: 'app-name-v2',
-            },
-          };
+              customMicroserviceName: 'app-name-v2'
+            }
+          }
 
-          const res = itHasAValidCustomMicroserviceName(input);
+          const res = itHasAValidCustomMicroserviceName(input)
 
-          expect(res).to.be.true;
-        });
-      });
+          expect(res).to.be.true
+        })
+      })
 
       context('but it is INVALID', () => {
         it('should return false', () => {
           const input = {
             config: {
-              customMicroserviceName: 'this is not a valid name',
-            },
-          };
+              customMicroserviceName: 'this is not a valid name'
+            }
+          }
 
-          const res = itHasAValidCustomMicroserviceName(input);
+          const res = itHasAValidCustomMicroserviceName(input)
 
-          expect(res).to.be.false;
-        });
-      });
-    });
-  });
+          expect(res).to.be.false
+        })
+      })
+    })
+  })
 
   describe('#getMicroserviceName', () => {
-    const packageLib = require('../lib/get-package-json');
+    const packageLib = require('../lib/get-package-json')
 
     context('when it DOES NOT have a custom name', () => {
       beforeEach(() => {
         box.stub(packageLib, 'getPackageJSON')
-          .callsFake(() => ({name: 'my-app', version: '1.2.0'}));
-      });
+          .callsFake(() => ({ name: 'my-app', version: '1.2.0' }))
+      })
 
       it('should return my-app-v1', () => {
-        const { getMicroserviceName } = cleanrequire('../');
-        const res = getMicroserviceName();
+        const { getMicroserviceName } = cleanrequire('../')
+        const res = getMicroserviceName()
 
-        expect(res).to.equal('my-app-v1');
-      });
-    });
+        expect(res).to.equal('my-app-v1')
+      })
+    })
 
     context('when it has a custom name', () => {
       context('and it is Valid', () => {
@@ -205,20 +204,20 @@ describe('index.js', () => {
             name: 'my-app',
             version: '1.2.0',
             config: {
-              customMicroserviceName: 'my-custom-name-v2',
-            },
-          };
+              customMicroserviceName: 'my-custom-name-v2'
+            }
+          }
           box.stub(packageLib, 'getPackageJSON')
-            .callsFake(() => packageObject);
-        });
+            .callsFake(() => packageObject)
+        })
 
         it('should return the value of config.customMicroserviceName', () => {
-          const { getMicroserviceName } = cleanrequire('../');
-          const res = getMicroserviceName();
+          const { getMicroserviceName } = cleanrequire('../')
+          const res = getMicroserviceName()
 
-          expect(res).to.equal('my-custom-name-v2');
-        });
-      });
+          expect(res).to.equal('my-custom-name-v2')
+        })
+      })
 
       context('but it is INVALID', () => {
         beforeEach(() => {
@@ -226,20 +225,20 @@ describe('index.js', () => {
             name: 'my-app',
             version: '1.2.0',
             config: {
-              customMicroserviceName: 'my-custom-name',
-            },
-          };
+              customMicroserviceName: 'my-custom-name'
+            }
+          }
           box.stub(packageLib, 'getPackageJSON')
-            .callsFake(() => packageObject);
-        });
+            .callsFake(() => packageObject)
+        })
 
         it('should return the value compose the name', () => {
-          const { getMicroserviceName } = cleanrequire('../');
-          const res = getMicroserviceName();
+          const { getMicroserviceName } = cleanrequire('../')
+          const res = getMicroserviceName()
 
-          expect(res).to.equal('my-app-v1');
-        });
-      });
-    });
-  });
-});
+          expect(res).to.equal('my-app-v1')
+        })
+      })
+    })
+  })
+})
